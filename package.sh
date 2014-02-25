f458540b9dc8a639c0b8e5f18abff12391cdb6da
Sam Bashton <sam@bashton.com>
2014-02-25 14:29:09 +0000

    Add missing dependencies

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 version=$1
 iterator=$2
-fpm -s dir -t rpm -n find-ec2 -v $1 --iteration $2 -a noarch -d 'python-pystache >= 0.5.3' find-ec2=/usr/bin/ templates=/etc/find-ec2/
+fpm -s dir -t rpm -n find-ec2 -v $1 --iteration $2 -a noarch -d 'python-pystache >= 0.5.3' -d 'python-argparse' find-ec2=/usr/bin/ templates=/etc/find-ec2/
