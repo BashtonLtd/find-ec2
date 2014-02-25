89e098e5a2cdf89dd5348f4dd5070626bee01a02
Sam Bashton <sam@bashton.com>
2014-02-25 11:56:17 +0000

    Versions for qnd packaging script

--- package.sh
+++ package.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-fpm -s dir -t rpm -n find-ec2 -v 0.1 --iteration 2 -a noarch -d 'python-pystache >= 0.5.3' find-ec2=/usr/bin/ templates=/etc/find-ec2/
+version=$1
+iterator=$2
+fpm -s dir -t rpm -n find-ec2 -v $1 --iteration $2 -a noarch -d 'python-pystache >= 0.5.3' find-ec2=/usr/bin/ templates=/etc/find-ec2/
